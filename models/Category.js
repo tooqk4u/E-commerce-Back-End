10dbeee67656be4833db81955e1ab63b9eba0831
tooqk4u <tooqk4u@protonmail.com>
2022-02-26 17:37:43 -0500

    routes for model tag added

--- models/Category.js
+++ models/Category.js
@@ -1,16 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+    // define category_name column
     category_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-  }
+            type: DataTypes.STRING,
+            allowNull: false
+        }
   },
   {
     sequelize,

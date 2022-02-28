0849913402f8f62714a44217db93a95c9fc451ff
tooqk4u <tooqk4u@protonmail.com>
2022-02-28 00:14:53 -0500

    editing models

--- models/Product.js
+++ models/Product.js
@@ -8,59 +8,52 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define id columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-  },
-  // define product name colums
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-  },
-  // define price column
-  price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-    }
-  },
-  // define stock column
-  stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define product_name column
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define price column
+        price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        // define stock column
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
             validate: {
                 isNumeric: true
             }
-  },
-  // define category_id column
-  category_id: {
-      // use the special Sequelize DataTypes object provide what type of data it is
-      type: DataTypes.INTEGER,
-      // turn on auto increment
-      autoIncrement: true,
-      // this is the equivalent of SQL's `NOT NULL` option
-          allowNull: false,
-          references: {
+        },
+        // define category_id column
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
                 model: "category",
                 key: "id"
             }
-      
-      
-  }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+    }
 );
 
 module.exports = Product;
